822783d2f54cf0ae337891bf0afc90485e0d2c54
Vinícius Marini <57442852+marinisz@users.noreply.github.com>
2020-06-01 10:09:25 -0300

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var url = 'http://newsapi.org/v2/top-headlines?' +
+          'sources=bbc-news&' +
+          'apiKey=API_KEY';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
