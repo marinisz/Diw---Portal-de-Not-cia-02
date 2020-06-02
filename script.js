f4525da2492f45d38bc22c0b78874b33c6e72a46
Vinícius Marini <57442852+marinisz@users.noreply.github.com>
2020-06-02 15:41:12 -0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+var API_KEY = 'ff42562d3e064cc689c7bf1600919125'
+function exibeNoticia() {
+    let diviTela = document.getElementById('buscador')
+    let texto = ' '
 
-var url = 'http://newsapi.org/v2/top-headlines?' +
-          'sources=bbc-news&' +
-          'apiKey=API_KEY';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+    //montar o texto
+    let dados = JSON.parse (this.responseText);
+    for (i = 0; i < dados.articles.length; i++) {
+        let noticia = dados.articles[i];
+        let data = new Date(noticia.publishedAt);
+        texto = texto + `
+        <div class="div1"><img src="${noticia.urlToImage}" alt="">
+                      <span class="titulo">${noticia.title}</span><br>
+                      <span class="credito"> ${data.toLocaleDateString()} - ${noticia.source.name} - ${noticia.author}</span><br>
+                      <span class="text">${noticia.content}<a href="${noticia.url}">Leia mais...</a> </span>
+                  </div>`;
+        //preencher
+        diviTela.innerHTML = texto;
+    }
+}
+
+    function executaPesquisa() {
+        let query = document.getElementById('txtPesquisa').value;
+        let xhr = new XMLHttpRequest();
+        xhr.onload = exibeNoticia;
+        xhr.open('GET', `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`)
+        xhr.send
+    }
+    document.getElementById('btnPesquisa').addEventListener('click', executaPesquisa);
